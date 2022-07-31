56087536fcd2e82e93d4a3f31c24efb35b263cb6
Syed Farhan Farogh <farhan.syed01@gmail.com>
2022-07-31 14:25:00 -0700

    delete unused file

--- src/main/java/networking/platform/stats/rest/data/MessageList.java
+++
@@ -1,8 +0,0 @@
-package networking.platform.stats.rest.data;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MessageList {
-	public List<Message> messages = new ArrayList<Message>();
-}
